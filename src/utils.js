3561877947f439aa983072a180d597a1d0e55093
Gwin Yim <march23hare@gmail.com>
2018-12-04 04:01:34 +0900

    initial

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+export const validDomainRegEX = new RegExp(/^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/);
+const threeMonthInMs = 7.884e+9;
+const twoMonthInMs = 5.256e+9;
+const oneMonthInMs = 2.628e+9;
+
+export function checkIsValidDomain(domain) {
+  return validDomainRegEX.test(domain);
+}
+
+export function getStartDateForSimilarWeb (now) {
+  const endDate = new Date(now.valueOf() - threeMonthInMs);
+  return `${endDate.getFullYear()}-${endDate.getMonth()}`;
+}
+
+export function getEndDateForSimilarWeb (now) {
+  const endDate = new Date(now.valueOf() - oneMonthInMs);
+  return `${endDate.getFullYear()}-${endDate.getMonth()}`;
+}
